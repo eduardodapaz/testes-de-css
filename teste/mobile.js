2657e27cdb109e42f699bfae2c2a5fe6a608622e
eduardodapaz <127500359+eduardodapaz@users.noreply.github.com>
2023-05-08 22:13:12 -0300

    Add files via upload

---
+++ teste/mobile.js
@@ -0,0 +1,8 @@
+class mobileNavbar{
+    constructor(mobilemenu,navlist,navLinks  ) {
+        this.mobile-menu = Document.querySelector(mobile-menu);
+        this.navlist = Document.querySelector(navlist);
+        this.navLinks = Document.querySelectorAll(navLinks);
+        this.actveClass = "actve" ;
+    }
+}
